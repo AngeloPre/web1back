6836547ba8c0591978a4ccf084d0af06aff951d9
AngeloPre <angelo.prebianca@gmail.com>
2024-11-18 03:03:11 -0300

    feat: :sparkles: server.ts added cors
    Added cors to server.ts so the frontend is allowed to consume it

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors'; // Import Fastify CORS plugin
 import { transactionController } from './controllers/transactionController'; // Ajuste o caminho conforme necessário
 
 const app = fastify({ logger: true });
 
 async function main() {
-  // Registra o controller
+  // Enable CORS
+  app.register(cors, {
+    origin: 'http://localhost:5173', // Allow requests from the frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+  });
+
+  // Register the controller
   app.register(transactionController);
 
-  // Inicia o servidor
+  // Start the server
   try {
     await app.listen({ port: 3333 });
     app.log.info(`Servidor rodando em: http://localhost:3333`);
